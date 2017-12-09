3450099fa4317f6230c0c92a6d0ac185167d815b
JustineVuillemot <vuillemotjustine@gmail.com>
2017-12-09 20:02:00 +0100

    base project

---
+++ cmake-build-debug/glimac/CMakeFiles/glimac.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/version1/test.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/__/version1/test.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Cone.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Cone.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/FreeflyCamera.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/FreeflyCamera.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Geometry.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Geometry.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Image.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Image.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Program.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Program.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/SDLWindowManager.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/SDLWindowManager.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Shader.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Shader.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/Sphere.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/Sphere.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/TrackballCamera.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/TrackballCamera.cpp.obj"
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/glimac/src/tiny_obj_loader.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/src/tiny_obj_loader.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/MinGW/include/SDL"
+  "../glimac/include"
+  "../third-party/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/version1/CMakeFiles/version1_SDLtemplate.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/version1/SDLtemplate.cpp" "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/version1/CMakeFiles/version1_SDLtemplate.dir/SDLtemplate.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/MinGW/include/SDL"
+  "../glimac/include"
+  "../third-party/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/justine/Documents/IMAC/cours/synthese-img/OpenGL/Projet2/cmake-build-debug/glimac/CMakeFiles/glimac.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
