f7ec1b1a4b49abbad5e0d722060519ec5b46c21f
JustineVuillemot <vuillemotjustine@gmail.com>
2017-12-26 16:52:07 +0100

    cube marche

--- IMACMAN/TEST_Cube.cpp
+++ IMACMAN/TEST_Cube.cpp
@@ -13,6 +13,7 @@
 #include <glimac/Program.hpp>
 #include <glimac/Image.hpp>
 #include <glimac/SDLWindowManager.hpp>
+#include <glimac/TrackballCamera.hpp>
 #include "Buffer.hpp"
 #include "VBO.hpp"
 #include "VAO.hpp"
@@ -23,7 +24,9 @@ using namespace glimac;
 
 int main(int argc, char** argv) {
     // Initialize SDL and open a window
-    SDLWindowManager windowManager(800, 600, "GLImac");
+    float width = 800;
+    float height = 800;
+    SDLWindowManager windowManager(width, height, "GLImac");
 
     // Initialize glew for OpenGL3+ support
     GLenum glewInitError = glewInit();
@@ -39,6 +42,11 @@ int main(int argc, char** argv) {
     Program program = loadProgram(applicationPath.dirPath() + "shaders/3D.vs.glsl",
                                   applicationPath.dirPath() + "shaders/tex3D.fs.glsl");
     program.use();
+
+    /*UNIFORMES*/
+    GLuint uMVMatrix = glGetUniformLocation(program.getGLId(), "uMVMatrix");
+    GLuint uMVPMatrix = glGetUniformLocation(program.getGLId(), "uMVPMatrix");
+    GLuint uNormalMatrix = glGetUniformLocation(program.getGLId(), "uNormalMatrix");
 
     VBO vbo = VBO();
     VAO vao = VAO();
@@ -57,6 +65,11 @@ int main(int argc, char** argv) {
 
     vao.remplirBuffer(vertices, &vbo, &ibo);
 
+    glm::mat4 modelViewMat, normalMat, mvProjMat;
+
+    TrackballCamera cam;
+    bool rotatebutton;
+
     // Application loop:
     bool done = false;
     while(!done) {
@@ -66,7 +79,32 @@ int main(int argc, char** argv) {
             if(e.type == SDL_QUIT) {
                 done = true; // Leave the loop after this iteration
             }
+            if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_WHEELUP){
+                cam.moveFront(1);
+            }
+            if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_WHEELDOWN){
+                cam.moveFront(-1);
+            }
+            if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_RIGHT){
+                rotatebutton = true;
+            }
+            if(e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_RIGHT){
+                rotatebutton = false;
+            }
+            if(e.type == SDL_MOUSEMOTION && rotatebutton){
+                cam.rotateLeft(e.motion.xrel);
+                cam.rotateUp(e.motion.yrel);
+            }
         }
+
+        modelViewMat = cam.getViewMatrix();
+        mvProjMat = glm::perspective(glm::radians(70.f), width/height, 0.1f, 100.f);
+        normalMat = glm::transpose(glm::inverse(modelViewMat));
+
+        /*UNFORMES*/
+        glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(modelViewMat));
+        glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE, glm::value_ptr(normalMat));
+        glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE, glm::value_ptr(mvProjMat*modelViewMat));
 
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -74,7 +112,7 @@ int main(int argc, char** argv) {
 
         // => On utilise glDrawElements à la place de glDrawArrays
         // Cela indique à OpenGL qu'il doit utiliser l'IBO enregistré dans le VAO
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         vao.debind();
 
